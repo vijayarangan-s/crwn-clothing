602af9e59c6abcb40e79b90d8350951c4855ad3c
Vijayarangan <vijayarangan.s3010@gmail.com>
2021-09-26 13:37:22 +0530

    User ablitiy to sign up using firebase

--- src/components/Pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up.component.jsx
+++ src/components/Pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import SignUp from '../../Sign-Up/SignUp.component'
 import SignIn from '../../SignIn/SignIn.component'
+import './Sign-In-And-Sign-Up.styles.scss'
 
 export const SignInAndSignUp = () => {
     return (
-        <div>
-            <h2>SIGN IN</h2>
+        <div className="sign-in-and-sign-up">
             <SignIn/>
+            <SignUp/>
         </div>
     )
 }
