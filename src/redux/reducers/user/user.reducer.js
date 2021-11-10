d6447813d85df14ec1b199700d8e0a9c969ccfd0
Vijayarangan <vijayarangan.s3010@gmail.com>
2021-11-10 22:04:40 +0530

    Implemented for redux sagas for Google Sign In Button

--- src/redux/reducers/user/user.reducer.js
+++ src/redux/reducers/user/user.reducer.js
@@ -1,17 +1,27 @@
 import { userActionType } from "../../types/user/user.type";
 
 const INITIAL_STATE = {
-    currentUser: null
-}
+  currentUser: null,
+  error: null
+};
 
 export const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type){
-        case userActionType.SET_CURRENT_USER:
-            return{
-                ...state,
-                currentUser:action?.payload?.currentUser
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case userActionType.GOOGLE_SIGN_IN_SUCCESS:
+    case userActionType.EMAIL_SIGN_IN_SUCCESS:
+        
+      return {
+        ...state,
+        currentUser: action?.payload,
+        error: null
+      };
+    case userActionType.GOOGLE_SIGN_IN_FAILURE:
+    case userActionType.EMAIL_SIGN_IN_FAILURE:
+        return {
+            ...state,
+            error: action?.payload
+        }
+    default:
+      return state;
+  }
+};
