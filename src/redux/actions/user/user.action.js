36992f87277772941c17c9a40509dae6316272f3
vijayarangan <75248380+vijayarangan-s@users.noreply.github.com>
2021-11-12 19:58:02 +0530

    Email Sign In Redux Saga

--- src/redux/actions/user/user.action.js
+++ src/redux/actions/user/user.action.js
@@ -1,39 +1,20 @@
 import { userActionType } from "../../types/user/user.type"
 
-export const setCurrentUser = (user) => {
-    return {
-        type: userActionType.SET_CURRENT_USER,
-        payload: user
-    }
-}
-
 export const googleSignInStart = () => ({
     type: userActionType.GOOGLE_SIGN_IN_START
 })
 
-export const googleSignInSuccess = user => ({
-    type:userActionType.GOOGLE_SIGN_IN_SUCCESS,
+export const emailSignInStart = (emailAndPassword) => ({
+    type: userActionType.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword
+  });
+
+export const signInSuccess = user => ({
+    type:userActionType.SIGN_IN_SUCCESS,
     payload: user
 })
 
-export const googleSignInFailure = error => ({
-    type: userActionType.GOOGLE_SIGN_IN_FAILURE,
-    payload: error
-})
-
-export const emailSignInStart = (emailAndPassword) => {
-    return {
-        type: userActionType.EMAIL_SIGN_IN_START,
-        payload: emailAndPassword
-    }
-}
-
-export const emailSignInSuccess = user => ({
-    type:userActionType.EMAIL_SIGN_IN_SUCCESS,
-    payload: user
-})
-
-export const emailSignInFailure = error => ({
-    type: userActionType.EMAIL_SIGN_IN_FAILURE,
+export const signInFailure = error => ({
+    type: userActionType.SIGN_IN_FAILURE,
     payload: error
 })
