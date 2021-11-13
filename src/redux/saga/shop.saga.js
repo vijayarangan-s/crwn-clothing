7ad4213a66d257f0ad85588c0bb36f5002a07d03
vijayarangan <75248380+vijayarangan-s@users.noreply.github.com>
2021-11-13 23:45:18 +0530

    saga

--- src/redux/saga/shop.saga.js
+++ src/redux/saga/shop.saga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, all,put } from "redux-saga/effects";
 import { ShopActionType } from "../types/shop/shop.types";
 import {
   firestore,
@@ -25,9 +25,14 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  console.log("saga...");
   yield takeLatest(
     ShopActionType.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSaga(){
+  yield(all([
+    call(fetchCollectionsAsync)
+  ]))
 }
