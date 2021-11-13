29a36399541a82cf36b240032a432db1554fa991
vijayarangan <75248380+vijayarangan-s@users.noreply.github.com>
2021-11-13 23:44:37 +0530

    saga

--- src/redux/saga/root-saga.js
+++ src/redux/saga/root-saga.js
@@ -1,7 +1,12 @@
 import { all, call } from "@redux-saga/core/effects";
-import { fetchCollectionsStart } from "./shop.saga";
+import { cartSaga } from "./cart.saga";
+import { shopSaga } from "./shop.saga";
 import { userSaga } from "./user.saga";
 
 export default function* rootSaga(){
-    yield all([call(fetchCollectionsStart), call(userSaga)])
+    yield all([
+        call(shopSaga), 
+        call(userSaga),
+        call(cartSaga)
+    ])
 }
